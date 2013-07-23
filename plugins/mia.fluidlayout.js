38bfde24a22b54a478dd6618bab385ef4454bbc3
Myachin <i.myachin@ugmk.com>
2013-07-23 21:21:49 +0600

    fluid layout upgrade

--- plugins/mia.fluidlayout.js
+++ plugins/mia.fluidlayout.js
@@ -14,12 +14,25 @@
     };
 
     var setupsplitter = function(layout) {
-        var splitter = $('<div class="layout-splitter"/>');
+        var splitter = $('<div class="fluid-splitter"/>');
         var ch = layout.children();
         $.each(ch, function(i, el) {
             el = $(el);
-            el.addClass("fluid-part" + (i + 1));
             el.addClass("fluid-part");
+            var s = splitter.clone();
+            el.prepend(s);
+            el.prepend($('<div class="fluid-legend"/>').text(el.attr("legend") || "Скрытая панель"));
+            s.click(function(e) {
+                var el = $(e.target);
+                var parent = el.parent();
+                el.nextAll().toggle();
+                parent.toggleClass("fluid-part");
+                parent.toggleClass("fluid-part-hidden");
+                
+            });
+            el.on('click', 'legend', function(e) {
+                $(e.target).nextAll().toggle();
+            });
         });
     };
     $.extend($.fn, { fluidlayout : fluidlayout });
